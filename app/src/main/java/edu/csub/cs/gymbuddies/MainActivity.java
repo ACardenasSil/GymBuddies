2a6eda460602af4b72dcf362fd1014e1f8cc30f5
ACardenasSil <alsillasedu@gmail.com>
2022-11-27 17:59:02 -0800

    Committing web-based Android app.

---
+++ app/src/main/java/edu/csub/cs/gymbuddies/MainActivity.java
@@ -0,0 +1,24 @@
+package edu.csub.cs.gymbuddies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://www.cs.csub.edu/~gymbuddies/GymBuddies/Login.php");
+        //myWebView.loadUrl("file:///android_asset/HTML/Notes.html");
+
+    }
+}
